b0029f62935e6dc5cd27fde31aebbe56e64e1501
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-01-25 11:14:15 -0300

    Adicionando arquivo

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$items = array(
+    array ("name" => "Lápis", "weight" => 10, "utility" => 269),
+    array ("name" => "Caneta", "weight" => 55, "utility" => 95),
+    array ("name" => "Borracha", "weight" => 10, "utility" => 4),
+    array ("name" => "Apontador", "weight" => 47, "utility" => 60),
+    array ("name" => "Caderno", "weight" => 5, "utility" => 32),
+    array ("name" => "Livro", "weight" => 4, "utility" => 23), 
+    array ("name" => "Tesoura", "weight" => 50, "utility" => 72),
+    array ("name" => "Lapiseira", "weight" => 8, "utility" => 80),
+    array ("name" => "Grafite", "weight" => 61, "utility" =>62),
+    array ("name" => "Régua", "weight" => 85, "utility" => 65),
+    array ("name" => "Pasta", "weight" => 87, "utility" => 46)
+);
+
+//print_r($items);
+
+$maxWeight = 250;
+
+function bestOrganization($items, $maxWeight)
+{
+
+}
+
+?>
