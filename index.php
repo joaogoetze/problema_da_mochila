616d73196c705a6542f5d5c41f02f9236a14890d
João <joaoaugustoferreira5@mail.com>
2024-01-25 17:42:59 -0300

    Adicionandos códigos

--- index.php
+++ index.php
@@ -1,26 +1,92 @@
 <?php
 
-$items = array(
-    array ("name" => "Lápis", "weight" => 10, "utility" => 269),
-    array ("name" => "Caneta", "weight" => 55, "utility" => 95),
-    array ("name" => "Borracha", "weight" => 10, "utility" => 4),
-    array ("name" => "Apontador", "weight" => 47, "utility" => 60),
-    array ("name" => "Caderno", "weight" => 5, "utility" => 32),
-    array ("name" => "Livro", "weight" => 4, "utility" => 23), 
-    array ("name" => "Tesoura", "weight" => 50, "utility" => 72),
-    array ("name" => "Lapiseira", "weight" => 8, "utility" => 80),
-    array ("name" => "Grafite", "weight" => 61, "utility" =>62),
-    array ("name" => "Régua", "weight" => 85, "utility" => 65),
-    array ("name" => "Pasta", "weight" => 87, "utility" => 46)
+$itens = array(
+    array ("peso" => 5, "valor" => 5),
+    array ("peso" => 4, "valor" => 4),
+    array ("peso" => 6, "valor" => 6),
 );
 
-//print_r($items);
+bestOrganization($itens);
 
-$maxWeight = 250;
+function bestOrganization($itens)
+{
+    $peso_limite = 10;
+    $peso_sobrando = 10;
+    $recorde_valor = 0;
+    $melhor_array = array();
 
-function bestOrganization($items, $maxWeight)
+    $tamanhoArray = count($itens);
+    
+    for($x=0; $x<$tamanhoArray; $x++)
+    {
+        if($itens[$x]["peso"]<$peso_limite)
+        {
+            if($itens[$x]["peso"])
+            {
+                
+            }
+        }
+        
+        
+        
+        
+        $peso = $itens[$x]["peso"];
+        echo $peso;
+    }
+
+    /*foreach($itens as $item)
+    {
+        if($item['peso'] <= $peso_sobrando)
+        {
+            array_push($melhor_array, $item);
+            $peso_sobrando = $peso_sobrando - $item['peso'];
+            $recorde_valor = $recorde_valor + $item['valor'];
+        }
+    }
+    print_r($melhor_array);
+    echo "<BR>";
+    echo "Peso sobrando: " . $peso_sobrando . "<br>";
+    echo "Valor: " . $recorde_valor;
+    */
+}
+
+
+
+
+/*
+$items = array(
+    array ("name" => "Lápis", "peso" => 10, "valor" => 269),
+    array ("name" => "Caneta", "peso" => 55, "valor" => 95),
+    array ("name" => "Borracha", "peso" => 10, "valor" => 4),
+    array ("name" => "Apontador", "peso" => 47, "valor" => 60),
+    array ("name" => "Caderno", "peso" => 5, "valor" => 32),
+    array ("name" => "Livro", "peso" => 4, "valor" => 23), 
+    array ("name" => "Tesoura", "peso" => 50, "valor" => 72),
+    array ("name" => "Lapiseira", "peso" => 8, "valor" => 80),
+    array ("name" => "Grafite", "peso" => 61, "valor" =>62),
+    array ("name" => "Régua", "peso" => 85, "valor" => 65),
+    array ("name" => "Pasta", "peso" => 87, "valor" => 46)
+);
+*/
+
+/* Adiciona um item após o outro até lotar
+function bestOrganization($items)
 {
 
+    $maxpeso = 250;
+    $available = 250;
+    $backPack = array();
+
+    foreach($items as $item)
+    {
+        if($item['peso'] <= $available)
+        {
+            array_push($backPack, $item);
+            $available = $available - $item['peso'];
+        }
+    }
+    echo json_encode($backPack);
 }
+*/
 
 ?>
