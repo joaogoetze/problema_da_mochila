fda0bdfba5c40dba8331e0c53ea15fe890371777
João <joaoaugustoferreira5@mail.com>
2024-01-29 17:56:01 -0300

    Adicionando versão do Alison

--- index.php
+++ index.php
@@ -1,92 +1,55 @@
 <?php
 
-$itens = array(
-    array ("peso" => 5, "valor" => 5),
-    array ("peso" => 4, "valor" => 4),
-    array ("peso" => 6, "valor" => 6),
-);
+    $totalMochila = 250;
+    $pesoMochila = 0;
+    $utility = 0;    
+    $newItems = array();
+    $newArray = array();
 
-bestOrganization($itens);
-
-function bestOrganization($itens)
-{
-    $peso_limite = 10;
-    $peso_sobrando = 10;
-    $recorde_valor = 0;
-    $melhor_array = array();
-
-    $tamanhoArray = count($itens);
-    
-    for($x=0; $x<$tamanhoArray; $x++)
-    {
-        if($itens[$x]["peso"]<$peso_limite)
-        {
-            if($itens[$x]["peso"])
-            {
-                
-            }
-        }
-        
-        
-        
-        
-        $peso = $itens[$x]["peso"];
-        echo $peso;
-    }
-
-    /*foreach($itens as $item)
-    {
-        if($item['peso'] <= $peso_sobrando)
-        {
-            array_push($melhor_array, $item);
-            $peso_sobrando = $peso_sobrando - $item['peso'];
-            $recorde_valor = $recorde_valor + $item['valor'];
-        }
-    }
-    print_r($melhor_array);
-    echo "<BR>";
-    echo "Peso sobrando: " . $peso_sobrando . "<br>";
-    echo "Valor: " . $recorde_valor;
-    */
-}
-
-
-
-
-/*
-$items = array(
-    array ("name" => "Lápis", "peso" => 10, "valor" => 269),
-    array ("name" => "Caneta", "peso" => 55, "valor" => 95),
-    array ("name" => "Borracha", "peso" => 10, "valor" => 4),
-    array ("name" => "Apontador", "peso" => 47, "valor" => 60),
-    array ("name" => "Caderno", "peso" => 5, "valor" => 32),
-    array ("name" => "Livro", "peso" => 4, "valor" => 23), 
-    array ("name" => "Tesoura", "peso" => 50, "valor" => 72),
-    array ("name" => "Lapiseira", "peso" => 8, "valor" => 80),
-    array ("name" => "Grafite", "peso" => 61, "valor" =>62),
-    array ("name" => "Régua", "peso" => 85, "valor" => 65),
-    array ("name" => "Pasta", "peso" => 87, "valor" => 46)
-);
-*/
-
-/* Adiciona um item após o outro até lotar
-function bestOrganization($items)
-{
-
-    $maxpeso = 250;
-    $available = 250;
-    $backPack = array();
+    $items = array(
+        array ("name" => "Lápis", "weight" => 10, "utility" => 269),
+        array ("name" => "Caneta", "weight" => 55, "utility" => 95),
+        array ("name" => "Borracha", "weight" => 10, "utility" => 4),
+        array ("name" => "Apontador", "weight" => 47, "utility" => 60),
+        array ("name" => "Caderno", "weight" => 5, "utility" => 32),
+        array ("name" => "Livro", "weight" => 4, "utility" => 23), 
+        array ("name" => "Tesoura", "weight" => 50, "utility" => 72),
+        array ("name" => "Lapiseira", "weight" => 8, "utility" => 80),
+        array ("name" => "Grafite", "weight" => 61, "utility" =>62),
+        array ("name" => "Régua", "weight" => 85, "utility" => 65),
+        array ("name" => "Pasta", "weight" => 87, "utility" => 46)
+    );
 
     foreach($items as $item)
     {
-        if($item['peso'] <= $available)
+        //Esse cálculo compara o peso pela utilidade, quanto maior o resultado, maior é a necessidade desse item estar na mochila
+        $dif = ($item["utility"] / $item["weight"]);
+        //São adicionados em um novo array os valores + cálculo anterior     
+        array_push($newItems, [$item["name"], $item["weight"], $item["utility"], $dif]);
+    }
+
+    //Ordena esse array em ordem decrescente, tomando como base o valor do dif
+    array_multisort(array_column($newItems, 3), SORT_DESC, $newItems);
+
+    //Pega os valores necessários até estourar o peso
+    foreach($newItems as $mochila)
+    {
+        if (($pesoMochila + $mochila[1]) <= $totalMochila)
         {
-            array_push($backPack, $item);
-            $available = $available - $item['peso'];
+            $pesoMochila += $mochila[1];
+            $utility += $mochila[2]; 
+            array_push($newArray, $mochila[0]);
         }
     }
-    echo json_encode($backPack);
-}
-*/
 
+    echo "Best Solution: <br>";
+    echo "Weight: " . $pesoMochila . "<br>";
+    echo "Utility: " . $utility . "<br>";
+    echo "Itens included: <br><br>";
+
+    foreach($newArray as $itemMochila)
+    {
+        echo $itemMochila . " Included! <br>";
+    }
+    
 ?>
