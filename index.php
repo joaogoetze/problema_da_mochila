1833b43c128d7ba02ece078a73a7b07fba553016
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-01-29 21:41:32 -0300

    Puxando dados do arquivo CSV

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 <?php
 
+//PEQUENA MUDANÇA REALIZADA NO ARQUIVO CSV:
+//ADICIONEI O NOME DO ITEM ANTES DOS VALORES
+
+// Caminho para o arquivo CSV
+$csvFile = './data/dados.csv';
+
+// Ler o arquivo e armazenar cada linha em um array
+$lines = file($csvFile, FILE_IGNORE_NEW_LINES);
+
+// Iterar sobre cada linha
+foreach ($lines as $line) {
+    // Dividir a linha em itens usando o espaço como delimitador
+    $data = explode(' ', $line);
+    $items = array();
+
+    echo $data[0];
+    echo $data[1];
+    echo $data[2];
+
+    array_push($items, array ("name" => $data[0], "weight" => $data[1], "utility" => $data[2]));
+    // $items agora é um array contendo os itens da linha
+    // Faça o que quiser com esses itens
+    foreach ($data as $item) {
+        
+        
+            
+        
+        //echo $item . " aaaaaaaaaaaaaaaaaaaaaaaaa";
+    }
+    echo "<br>";
+    
+}
+
     $totalMochila = 250;
     $pesoMochila = 0;
     $utility = 0;    
     $newItems = array();
     $newArray = array();
-
+/*
     $items = array(
         array ("name" => "Lápis", "weight" => 10, "utility" => 269),
         array ("name" => "Caneta", "weight" => 55, "utility" => 95),
@@ -19,7 +52,7 @@
         array ("name" => "Régua", "weight" => 85, "utility" => 65),
         array ("name" => "Pasta", "weight" => 87, "utility" => 46)
     );
-
+*/ 
     foreach($items as $item)
     {
         //Esse cálculo compara o peso pela utilidade, quanto maior o resultado, maior é a necessidade desse item estar na mochila
@@ -37,7 +70,8 @@
         if (($pesoMochila + $mochila[1]) <= $totalMochila)
         {
             $pesoMochila += $mochila[1];
-            $utility += $mochila[2]; 
+            $utility += $mochila[2];
+            //Adiciona o item no array que será usado pra imprimir
             array_push($newArray, $mochila[0]);
         }
     }
@@ -51,5 +85,5 @@
     {
         echo $itemMochila . " Included! <br>";
     }
-    
+  
 ?>
